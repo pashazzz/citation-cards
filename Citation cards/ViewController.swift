67bdebb8d6436b7dcdb0df75994f07f0705a79a0
Pavlo Malyshkin <pmalyshkin@gmail.com>
2023-10-31 15:47:14 +0200

    Initial Commit

---
+++ Citation cards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Citation cards
+//
+//  Created by Pavlo Malyshkin on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
