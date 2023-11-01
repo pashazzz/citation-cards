ee85e1017983ad3fc5b925761cbb9ee7a99b8a97
Pavlo Malyshkin <pmalyshkin@gmail.com>
2023-11-01 17:21:01 +0200

    Adding icons for tabs and link screens with Controllers

--- Citation cards/Controllers/HomeController.swift
+++ Citation cards/Controllers/HomeController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "Home"
     }
 
 
--- Citation cards/Controllers/SettingsController.swift
+++ Citation cards/Controllers/SettingsController.swift
@@ -12,7 +12,7 @@ class SettingsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Settings"
     }
     
 
