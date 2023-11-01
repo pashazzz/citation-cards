4ba2730139aff04b6eedbc465a1db83f93170888
Pavlo Malyshkin <pmalyshkin@gmail.com>
2023-11-01 17:08:04 +0200

    Separate Controllers to their group

---
+++ Citation cards/Controllers/FavouritesController.swift
@@ -0,0 +1,29 @@
+//
+//  FavouritesController.swift
+//  Citation cards
+//
+//  Created by Pavlo Malyshkin on 1.11.2023.
+//
+
+import UIKit
+
+class FavouritesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
