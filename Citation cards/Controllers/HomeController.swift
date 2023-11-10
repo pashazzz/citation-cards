4ba884a05e7c8f4dad06e6527299f6d9b938f41e
Pavlo Malyshkin <pmalyshkin@gmail.com>
2023-11-10 12:49:07 +0200

    Separate PopupNotification into a View class

--- Citation cards/Controllers/HomeController.swift
+++ Citation cards/Controllers/HomeController.swift
@@ -10,6 +10,7 @@ import UIKit
 class HomeController: UITableViewController, UIGestureRecognizerDelegate {
     let storage = Storage()
     let settings = Settings()
+    let notificationPopup = PopupNotification()
     
     var citations: [Citation] = []
     var sortOrder: SortOrder = .newestFirst
@@ -51,38 +52,10 @@ class HomeController: UITableViewController, UIGestureRecognizerDelegate {
             self.tableView.reloadData()
         }
     }
-    
-    // TODO: refactor: separate it into View class
-    private func displayPopup(caption: String) {
-        let popupView = UIView(frame: CGRect(x: 0, y: UIWindow().bounds.height - 132, width: self.view.bounds.width , height: 40))
-//        let popupView = UIView(frame: CGRect(x: 0, y: 620, width: self.view.bounds.width , height: 40))
-        popupView.backgroundColor = .gray
-        popupView.alpha = 1.0
-        popupView.layer.cornerRadius = 20
-        popupView.frame = (popupView.frame.insetBy(dx: 20, dy: 0))
-        popupView.bringSubviewToFront(self.view)
 
-        let label = UILabel(frame: popupView.bounds)
-        label.bounds = label.bounds.insetBy(dx: 4, dy: 4)
-        label.textColor = .white
-        label.text = caption
-        label.font = UIFont.systemFont(ofSize: 14)
-        label.textAlignment = .center
-        popupView.addSubview(label)
-        
-        UIView.transition(with: popupView, duration: 1.5, options: .curveEaseInOut, animations: {
-//            self.view.addSubview(popupView)
-            self.tabBarController?.view.addSubview(popupView)
-            popupView.center.y += 4
-        }, completion: { _ in
-            UIView.animate(withDuration: 1, delay: 1.0, options: .curveEaseInOut) {
-                popupView.center.y -= 4
-            } completion: { finished in
-                print("completed animation")
-                // remember to remove the popup when you're done!
-                popupView.removeFromSuperview()
-            }
-        })
+    private func displayPopup(withCaption: String) {
+        notificationPopup.setConnectedController(self)
+        notificationPopup.displayNotification(withCaption: withCaption)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -125,7 +98,7 @@ class HomeController: UITableViewController, UIGestureRecognizerDelegate {
         }
         
         UIPasteboard.general.string = citationString
-        displayPopup(caption: "Copied to clipboard")
+        displayPopup(withCaption: "Copied to clipboard")
     }
 
     // display cell
---
+++ Citation cards/Views/PopupNotification.swift
@@ -0,0 +1,65 @@
+//
+//  PopupNotification.swift
+//  Citation cards
+//
+//  Created by Pavlo Malyshkin on 10.11.2023.
+//
+
+import UIKit
+
+protocol PopupNotificationProtocol {
+    var connectToView: UIViewController? {get set}
+    var label: UILabel {get set}
+    var yFromBottom: CGFloat {get set}
+    var animationDuration: TimeInterval {get set}
+    var delayDuration: TimeInterval {get set}
+    func setConnectedController(_ controller: UIViewController)
+    func displayNotification(withCaption: String) -> Void
+}
+
+class PopupNotification: UIView, PopupNotificationProtocol {
+    internal var connectToView: UIViewController?
+    internal var label = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+    internal var yFromBottom: CGFloat = 132
+    internal var animationDuration: TimeInterval = 0.1
+    internal var delayDuration: TimeInterval = 2.8
+
+    init() {
+        super.init(frame: CGRect(x: 0, y: 0, width: 0 , height: 0))
+        self.backgroundColor = .gray
+        self.layer.cornerRadius = 20
+
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textAlignment = .center
+        self.addSubview(label)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setConnectedController(_ controller: UIViewController) {
+        // on connect set the sizes and bounds
+        self.connectToView = controller
+        self.frame = CGRect(x: 0, y: connectToView!.view.bounds.height - yFromBottom, width: connectToView!.view.bounds.width, height: 40)
+        self.frame = (self.frame.insetBy(dx: 20, dy: 0))
+        label.frame = self.bounds
+        label.bounds = label.bounds.insetBy(dx: 4, dy: 4)
+    }
+    
+    public func displayNotification(withCaption caption: String) {
+        label.text = caption
+        
+        UIView.transition(with: self, duration: animationDuration, options: .curveEaseInOut, animations: {
+            self.connectToView!.tabBarController?.view.addSubview(self)
+            self.center.y += 1
+        }, completion: { _ in
+            UIView.animate(withDuration: self.animationDuration, delay: self.delayDuration, options: .curveEaseInOut) {
+                self.center.y -= 1
+            } completion: { finished in
+                self.removeFromSuperview()
+            }
+        })
+    }
+}
